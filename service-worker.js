0164f2be6448929dbbaea490ad6b822578be83ba
ruipms <Ruijpms@gmail.com>
2026-08-18 11:10:15 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,11 +1,35 @@
+const CACHE_NAME = "inquilinos-v2";
+
+const FILES_TO_CACHE = [
+  "./",
+  "./index.html",
+  "./santa-barbara.html",
+  "./av-marconi.html",
+  "./capitao-roby.html",
+  "./vila-paulo-jorge.html",
+  "./av-eua.html",
+  "./manifest.json"
+];
+
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open("inquilinos-cache").then(cache => {
-      return cache.addAll([
-        "/inquilinos/",
-        "/inquilinos/index.html"
-      ]);
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(FILES_TO_CACHE);
     })
+  );
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.map(key => {
+          if (key !== CACHE_NAME) {
+            return caches.delete(key);
+          }
+        })
+      )
+    )
   );
 });
 
